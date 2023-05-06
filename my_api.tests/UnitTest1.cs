1adab7f1f5bc62175f0d164fafb733e24ac304df
Hendra <hendraog@gmail.com>
2023-05-06 23:54:17 +0700

    update

---
+++ my_api.tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Castle.Core.Logging;
+using Moq;
+using my_api.Controllers;
+
+namespace my_api.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Mock<ILogger> l = new Mock<ILogger>();
+
+        Mock<idao> m = new Mock<idao>();
+        m.Setup(x => x.generateRadom()).Returns("dfd");
+
+        var s = new WeatherForecastController(m.Object);
+
+        var res = s.Get();
+
+
+        Assert.True(res != null);
+    }
+}
